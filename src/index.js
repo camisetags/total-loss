0713e9f4728da649cacd0ec9377c4be6723337b6
Gabriel Seixas <gseixas13@hotmail.com>
2017-01-29 23:15:35 -0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import './styles/style.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
