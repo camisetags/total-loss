9d047ab63dcf55842a31144505f0cc64f9a5bf0a
Gabriel Seixas <gseixas13@hotmail.com>
2017-04-04 04:39:00 -0300

    Add php entry server config

--- manifest_creator.py
+++ manifest_creator.py
@@ -19,8 +19,21 @@ for f in js_files:
 
 for f in css_files:
     offline_manifest_file.write(u'{}/{}\n'.format('static/css', f))
-    
+
+offline_manifest_file.write(u'serviceWorkerRegister.js\n');
+offline_manifest_file.write(u'serviceWorker.js\n');
+offline_manifest_file.write(u'app.manifest\n');
+
 offline_manifest_file.close()
+
+
+server_entry = open('./build/index.php', 'w')
+server_entry.write(u'<?php include_once("index.html"); ?>')
+server_entry.close()
+
+server_entry = open('./build/composer.json', 'w')
+server_entry.write(u'\{\}')
+server_entry.close()
 
 print(css_files)
 print(js_files)
