486cb645ef6ff47241b1e80e0fd04dc5cafe1698
Gabriel Seixas <salesseixasgabriel@gmail.com>
2017-11-03 23:13:14 -0200

    Covered all lines on the test of deck select

--- test/pages/Home.snapshot.spec.jsx
+++ test/pages/Home.snapshot.spec.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import HomePage from '../../src/app/pages/Home';
 
 describe('Home Page', () => {
-  const App = () => (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={() => <HomePage />} />
-      </Switch>
-    </BrowserRouter>
-  );
-
   it('should render Home page', () => {
-    const rendered = renderer.create(<App />);
+    const rendered = renderer.create(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    );
     expect(rendered.toJSON()).toMatchSnapshot();
   });
 });
